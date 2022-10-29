cc215af1bef7ac5562ab64bfb3495c83d1e8284f
hsmkazemi <hsm.kazemi@gmail.com>
2022-10-29 15:43:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.tic_tac_toe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.tictactoe.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ClickOnImage(View view){
+        ImageView clickedImageView = (ImageView) view;
+        clickedImageView.setImageResource(R.drawable.oo);
+        clickedImageView.animate().alpha(0).setDuration(1000);
+
+    }
+}
