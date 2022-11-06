65fc068e08562e2eca287d9e01e5341ce2f5425c
Hesam <99284206+hsmkazemi@users.noreply.github.com>
2022-11-06 07:04:56 +0100

    starting the game

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-
-import com.example.tic_tac_toe.R;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Boolean activePlayer = true;
+    int [][] playTable = {{0,0,0},{0,0,0},{0,0,0}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +18,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
     public void ClickOnImage(View view){
         ImageView clickedImageView = (ImageView) view;
-        clickedImageView.setImageResource(R.drawable.oo);
-        clickedImageView.animate().alpha(0).setDuration(1000);
+        String imageTag = clickedImageView.getTag().toString();
+        String[] status = imageTag.split("");
+        int first = Integer.parseInt(status[0]);
+        int second = Integer.parseInt(status[1]);
+
+
+        if (activePlayer) {
+            if (playTable[first][second] < 1) {
+                clickedImageView.setImageResource(R.drawable.oo);
+                playTable[first][second] = 1;
+                activePlayer = false;
+            }
+
+        }else{
+            if (playTable[first][second] < 1) {
+                clickedImageView.setImageResource(R.drawable.xx);
+                playTable[first][second] = 2;
+                activePlayer = true;
+            }
+
+        }
+        clickedImageView.animate().alpha(1).setDuration(500);
+
+
 
     }
+
 }
